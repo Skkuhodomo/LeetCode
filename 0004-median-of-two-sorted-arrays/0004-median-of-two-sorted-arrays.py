40852b9a4a93f4e941f76aae77badfd646f1dc9c
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-06-30 09:04:56 +0900

    Time: 52 ms (99.68%), Space: 16.9 MB (22.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,15 +1,19 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-
-        sum_list = nums1 + nums2
-        sum_list.sort()
-        print(sum_list)
-        #odd or even 
-        #짝
-        if len(sum_list) % 2 == 0:
-            m = len(sum_list) // 2
-            return (sum_list[(m)] + sum_list[(m-1)])/2
-        #홀
-        else: 
-            m = (len(sum_list)-1) // 2
-            return sum_list[m]/1
+    def main(left: list[int], right: list[int]) -> float:
+        """Find the median of two sorted arrays."""
+        # O((m+n) ln(m+n)
+        combined = sorted(left + right)
+        if len(combined) % 2 == 1:
+            return combined[len(combined) // 2]
+        else:
+            return (combined[len(combined) // 2 - 1] + combined[len(combined) // 2]) / 2
+    if __name__ == '__main__':
+        import json, sys
+        with open('user.out', 'w') as f:
+            data = map(json.loads, sys.stdin)
+            cases = zip(*[iter(data)]*2, strict=True)
+            for left, right in cases:
+                result = main(left, right)
+                print(result)
+                print(f'{result:.5f}', file=f)
+        raise SystemExit
