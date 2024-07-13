aa6497254755a11dee915b2b00005ab9fef2bbcd
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-13 22:59:40 +0900

    Time: 92 ms (98.05%), Space: 17.7 MB (16.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,8 +5,8 @@ class Solution:
         next_max_reach = 0
         
         for i in range(len(nums) - 1):
-            next_max_reach = max(next_max_reach, i + nums[i])
-            if i == cur_max_reach:
+            next_max_reach = max(next_max_reach, nums[i]+ i ) # 한번에 최대로 점프 
+            if i == cur_max_reach: 
                 jumps += 1
                 cur_max_reach = next_max_reach
                 
