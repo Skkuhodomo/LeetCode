134871a4e4ae46cb3e177b07c3e15119e282b9bc
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-13 20:01:52 +0900

    Time: 163 ms (86.56%), Space: 18 MB (55.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Sorting method
-        # O(nlogn)
+        # 절반 이상 --> 즉 중앙값이 최빈값이 됩니다. 
         nums.sort()
         n = len(nums)
         return nums[n//2]
