9baa202c5356677b1f78a976c47eb7ac97a2c216
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-28 01:39:40 +0900

    Time: 1628 ms (27.04%), Space: 61.7 MB (12.91%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -2,7 +2,7 @@ class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         result  =  set() #결과 판단을 위한 집합 
         for t in triplets: 
-            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]: #타겟보다 각각 모두 큰 경우 
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]: #타겟보다 세개 중 하나라도 클 경우. 가져가기 
                 continue
             for i, v in enumerate(t):
                 if v == target[i]:
