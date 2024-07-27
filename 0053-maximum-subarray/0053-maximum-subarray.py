3e1f1c84686b575834807aa2ced524a33f33933d
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-28 02:08:34 +0900

    Time: 553 ms (27.05%), Space: 31 MB (55.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,8 +1,8 @@
 #(Kadane's Algorithm)
 class Solution:
     def maxSubArray(self, nums):
-        cur_max, max_till_now = 0, -inf
+        cur_max, total_max = 0, -inf
         for c in nums:
             cur_max = max(c, cur_max + c)
-            max_till_now = max(max_till_now, cur_max)
-        return max_till_now
+            total_max = max(total_max, cur_max)
+        return total_max
