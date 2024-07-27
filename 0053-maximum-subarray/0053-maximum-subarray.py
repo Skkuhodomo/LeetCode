c0d39fb99ad3e582265e8569b91387486aca77b8
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-28 02:12:15 +0900

    Time: 548 ms (33.54%), Space: 31.1 MB (10.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,4 +1,7 @@
 #(Kadane's Algorithm)
+'''
+첫 인덱스에서 시작해서 하나씩 가며, 처음이 음수가 아니도록 쭉 훑고 지나감
+'''
 class Solution:
     def maxSubArray(self, nums):
         cur_max, total_max = 0, -inf
@@ -6,3 +9,4 @@ class Solution:
             cur_max = max(c, cur_max + c)
             total_max = max(total_max, cur_max)
         return total_max
+
