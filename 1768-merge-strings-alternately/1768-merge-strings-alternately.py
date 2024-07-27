199437c628e7dce20d1c41070e8635a65d98c255
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-27 23:51:27 +0900

    Time: 32 ms (77.06%), Space: 16.7 MB (25.97%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,11 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        # 더 긴 길이의 string을  high로 할당 
         if len(word1)>len(word2):
             high=word1
         else:
             high=word2
-        val=min(len(word1),len(word2))
-        res1=high[val:]
+        val=min(len(word1),len(word2)) # 둘중 더 짧은 길이 찾기 
+        cut_high=high[val:] # 긴 문자열을 더 짧은 길이 만큼 잘라주기 
+        print(cut_high)
         res=""
         i,j=0,0
         while i<len(word1) and j<len(word2):
@@ -13,7 +15,7 @@ class Solution:
             i+=1
             j+=1
         if len(word1) !=len(word2):
-            return (res+res1)
+            return (res+cut_high) 
         else:
             return (res)
                 
