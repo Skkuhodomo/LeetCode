b9524769dc78171e3fbdfd78aded7927748df560
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-07-28 00:24:00 +0900

    Time: 45 ms (78.27%), Space: 17.2 MB (93.69%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,7 +1,8 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vs = set("aeiouAEIOU")
-        v = [c for c in s if c in vs]
+        # 큐를 시용해 시간 복잡도가 O(n)인 알고리즘 
+        vs = set("aeiouAEIOU") # 모음의 집합 
+        v = [c for c in s if c in vs] # 모음 중, s에 있는 것 리스트로 
         res = ""
         for c in s:
             if c in vs:
