6ee19e3faa63bd056e29b345f6484ece20372c0b
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-09-07 18:47:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -8,33 +8,33 @@ class Solution:
             dictT[c] += 1
 
         required = len(dictT)
-        l, r = 0, 0
+        left, right = 0, 0
         formed = 0
 
         windowCounts = defaultdict(int)
         ans = [-1, 0, 0]
 
-        while r < len(s):
-            c = s[r]
+        while right < len(s):
+            c = s[right]
             windowCounts[c] += 1
 
             if c in dictT and windowCounts[c] == dictT[c]:
                 formed += 1
 
-            while l <= r and formed == required:
+            while left <= right and formed == required:
                 c = s[l]
 
-                if ans[0] == -1 or r - l + 1 < ans[0]:
-                    ans[0] = r - l + 1
-                    ans[1] = l
-                    ans[2] = r
+                if ans[0] == -1 or right - left + 1 < ans[0]:
+                    ans[0] = right - left + 1
+                    ans[1] = left
+                    ans[2] = right
 
                 windowCounts[c] -= 1
                 if c in dictT and windowCounts[c] < dictT[c]:
                     formed -= 1
 
-                l += 1
+                left += 1
 
-            r += 1
+            right += 1
 
         return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
