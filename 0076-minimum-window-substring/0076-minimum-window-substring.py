2fa610fea9f37fb9e9db66d0c4050f1e36522495
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-09-07 19:33:46 +0900

    Time: 90 ms (93.51%), Space: 17.3 MB (78.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,40 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
-            return ""
+        if t=="": return ""
 
-        dictT = defaultdict(int)
+        countT, window = {}, {}
+
         for c in t:
-            dictT[c] += 1
+            countT[c] = 1 + countT.get(c, 0)
+        have, need = 0, len(countT)
+        res, resLen=[-1, -1], float("infinity")
+        l = 0 
+        for r in range(len(s)):
+            c = s[r]
+            window[c]= 1 + window.get(c,0)
+            if c in countT and window[c] == countT[c]:
+                have +=1
+            while have == need:
+                if (r-l +1) < resLen:
+                    res = [l,r]
+                    resLen = (r-l+1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l+=1
 
-        required = len(dictT)
-        left, right = 0, 0
-        formed = 0
-
-        windowCounts = defaultdict(int)
-        ans = [-1, 0, 0]
-
-        while right < len(s):
-            c = s[right]
-            windowCounts[c] += 1
-
-            if c in dictT and windowCounts[c] == dictT[c]:
-                formed += 1
-
-            while left <= right and formed == required:
-                c = s[left]
-
-                if ans[0] == -1 or right - left + 1 < ans[0]:
-                    ans[0] = right - left + 1
-                    ans[1] = left
-                    ans[2] = right
-
-                windowCounts[c] -= 1
-                if c in dictT and windowCounts[c] < dictT[c]:
-                    formed -= 1
-
-                left += 1
-
-            right += 1
-
-        return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
+        l , r = res
+        return s[l:r+1] if resLen!= float("infinity") else ""
+        
